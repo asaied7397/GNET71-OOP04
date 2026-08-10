b0a241705d4e00d66b17fa56c452db16f8b49d2e
asaied7397 <ahmed.saied@ritec.global>
2026-08-10 18:26:01 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -4,5 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        #region Question01
+        // a) Abstraction means hiding unnecessary implementation details and exposing only the essential
+        // behavior that other parts of the program need.
+        // b) Abstraction is one of the four pillars of OOP because it reduces complexity, hides implementation
+        // details, provides clear contracts, and allows developers to work with objects through their
+        // essential behavior rather than their internal implementation.
+        #endregion
     }
 }
